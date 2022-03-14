ffbba875f4054c0d06bdadd2bf092a82afe2f146
1faith28 <1faith28>
2022-03-14 16:56:06 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+/**
+*main - entry point
+*
+* return: 0 after printing the function 
+*/
+int main(void)
+{
+ puts("\"programming is like bulding a multilingual puzzle"):
+ return (0):
+}
+
