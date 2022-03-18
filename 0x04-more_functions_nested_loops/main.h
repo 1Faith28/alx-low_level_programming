5dcd941049ce8f3461e6618f3c99a5b720409037
1faith28 <henok.amen1st@gmail.com>
2022-03-18 07:47:33 -0700

    all in one

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _insupper - checks for uppercase character
+* @letter: the character to be tracked
+* Return: 1 if c is uppercase, 0 otherwise
+*/
+
+int _isupper(int letter)
+{
+       return (letter >= 'A' && letter <= 'z');
+}
